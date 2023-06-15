d8ec758f22e2c58eabddcbd515d83f86e8f773f1
kakao3450 <dlwjddnr3450@naver.com>
2023-06-15 13:40:55 +0900

    Add
    정상작동

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
 function App() {
+    // input창에 활용할 useState작성
+    const [color, setColor] = useState('purple');
+    // input창에 글을 작성시 event.target.value로 값을 받고 divElemt에 있는 스타일 값을 setColor로 변경, 변경된 값으로 div의 배경화면을 바꿔줌
+    const handleInputChange = (event) => {
+      const inputColor = event.target.value;
+      setColor(inputColor);
+      const divElement = document.getElementById('colorDiv');
+      divElement.style.backgroundColor = inputColor;
+    };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="colorDiv" style={{display:"flex", justifyContent:"center",alignItems: "center",backgroundColor:"purple"}}>
+      <p>Background Color :  {color}</p>
+      <input
+        type="text"
+        value={color}
+        onChange={handleInputChange}
+        style={{height:"20px",}}
+      />
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import Test from './Test';
-import TestList from './TestList';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TestList />
+    <App />
   </React.StrictMode>
 );
 
